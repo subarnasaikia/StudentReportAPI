3edfebb6ff91afd24630ba68c27ec4b7541f60c9
Subarna Saikia <saikiasubarna74@gmail.com>
2025-04-14 22:12:17 +0530

    setup express app - app is connected with db and port

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from "dotenv";
 import { connectDB } from "./db/connect.db.js";
+import { app } from "./app.js";
+
+const PORT = process.env.PORT || 3000;
 
 dotenv.config({
     path: "./env",
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.error("Server error", error);
+            throw error;
+        });
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Error connecting to the database", error);
+        process.exit(1);
+    });
