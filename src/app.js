7841c7ca74cbc1b5cc8407331dae441670d23ccc
Subarna Saikia <saikiasubarna74@gmail.com>
2025-04-14 23:35:01 +0530

    Set middleware with express app - enable CORS - parse JSON payloads - parse URL-encoded payloads - setup the static file path - parse cookies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,43 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+// initialize express app
 const app = express();
+
+// set up middleware
+// enable CORS
+// allow requests from the specified origin
+// and allow credentials to be sent
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+// parse incoming requests with JSON payloads
+// and limit the payload size to 16kb
+app.use(
+    express.json({
+        limit: "16kb",
+    })
+);
+
+// parse incoming requests with URL-encoded payloads
+// and limit the payload size to 16kb
+// and extended to true to allow for rich objects and arrays
+app.use(
+    express.urlencoded({
+        extended: true,
+        limit: "16kb",
+    })
+);
+
+// serve static files from the public directory
+app.use(express.static("public"));
+
+// parse cookies from incoming requests
+app.use(cookieParser());
 
 export { app };
